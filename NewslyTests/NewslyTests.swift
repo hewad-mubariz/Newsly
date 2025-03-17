c5c899ad042acfee55719642942f51d2a302d091
Hewad <hewadmubariz@gmail.com>
2025-03-17 23:45:06 +0100

    Initial Commit

---
+++ NewslyTests/NewslyTests.swift
@@ -0,0 +1,17 @@
+//
+//  NewslyTests.swift
+//  NewslyTests
+//
+//  Created by Hewad Mubariz on 17.03.25.
+//
+
+import Testing
+@testable import Newsly
+
+struct NewslyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
